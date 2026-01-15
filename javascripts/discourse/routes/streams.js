643257626060cb4b1eb2a1c7821bf19e0a5a40e7
heartbeatpleasure <248991679+heartbeatpleasure@users.noreply.github.com>
2026-01-15 13:54:14 +0100

    update visual view

--- javascripts/discourse/routes/streams.js
+++ javascripts/discourse/routes/streams.js
@@ -1,11 +1,14 @@
 import DiscourseRoute from "discourse/routes/discourse";
 import { ajax } from "discourse/lib/ajax";
+import I18n from "I18n";
+import { themePrefix } from "discourse-common/lib/get-owner";
 
 export default class StreamsRoute extends DiscourseRoute {
+  titleToken() {
+    return I18n.t(themePrefix("streamers.title"));
+  }
+
   model() {
-    return ajax("/streams.json").catch(() => {
-      // Simpele fout-afhandeling voor nu
-      return { error: true };
-    });
+    return ajax("/streams.json");
   }
 }
