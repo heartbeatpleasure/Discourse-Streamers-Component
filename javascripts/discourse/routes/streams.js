7b32bf7046442e3cdcaece2cc1f2c6aac665f37e
heartbeatpleasure <248991679+heartbeatpleasure@users.noreply.github.com>
2026-01-15 17:25:47 +0100

    fix template en component

--- javascripts/discourse/routes/streams.js
+++ javascripts/discourse/routes/streams.js
@@ -1,14 +1,10 @@
+// javascripts/discourse/routes/streams.js
 import DiscourseRoute from "discourse/routes/discourse";
 import { ajax } from "discourse/lib/ajax";
-import I18n from "I18n";
-import { themePrefix } from "discourse-common/lib/get-owner";
 
 export default class StreamsRoute extends DiscourseRoute {
-  titleToken() {
-    return I18n.t(themePrefix("streamers.title"));
-  }
-
   model() {
+    // Backend endpoint van de plugin
     return ajax("/streams.json");
   }
 }
